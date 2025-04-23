7337afc0f32313898cab947af0ab0205203281ff
sajeebro <sajithmadusanka720@gmail.com>
2025-04-23 11:03:27 +0530

    docker running v1

--- src/main.ts
+++ src/main.ts
@@ -3,24 +3,30 @@ import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
 import { AUTH_PACKAGE_NAME } from './common/types/auth';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
-  // grpc server
+  void ConfigModule.forRoot({ isGlobal: true });
+  const url = process.env.AUTH_SERVICE_URL || 'localhost:50051';
+
+  const protoPath =
+    process.env.GRPC_PROTO_PATH || join(__dirname, '../auth.proto');
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
       transport: Transport.GRPC,
       options: {
-        protoPath: join(__dirname, '../auth.proto'),
+        protoPath,
         package: AUTH_PACKAGE_NAME,
-        url: 'localhost:50051', // 0.0.0.0
+        url: url,
       },
     },
   );
+
   app.enableShutdownHooks();
   await app.listen();
-  console.log(
-    `Auth service is running on: http://localhost:50051/${AUTH_PACKAGE_NAME}`,
-  );
+
+  console.log(`Auth service is running on ${url}`);
 }
 void bootstrap();
